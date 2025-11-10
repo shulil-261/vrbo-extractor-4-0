05d8ce02aba5d9bcfde2b918e04c7b333a6da28a
shulil-261 <shulilyquist2617127@gmail.com>
2025-11-10 09:05:39 -0700

    feat: add src/config/constants.py

---
+++ src/config/constants.py
@@ -0,0 +1,5 @@
+SEARCH_BASE_URL = "https://www.vrbo.com/search/keywords:{query}"
+DETAIL_USER_AGENT = "Mozilla/5.0 (compatible; VRBO-Extractor/4.0; +https://bitbash.dev)"
+
+INCLUDE_FLAGS = {"reviews", "gallery", "availability", "policies", "faq", "offers"}
+DEFAULT_LIMIT = 25
